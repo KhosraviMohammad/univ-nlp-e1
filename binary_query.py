9d84e0625eec434e605345b980260c9f191c3874
Mohammad Khosravi <mohammad.khosravi.7813@gmail.com>
2024-12-10 15:55:40 +0330

    improving: binary_query consider parentheses and operator precedence

--- binary_query.py
+++ binary_query.py
@@ -1,25 +1,48 @@
-def process_query(query, inverted_index):
+def evaluate_operator(operator, left_set, right_set):
+    """عملگر را اجرا می‌کند."""
+    if operator == "and":
+        return left_set & right_set
+    elif operator == "or":
+        return left_set | right_set
+    elif operator == "not":
+        return left_set - right_set
+    return set()
+
+def process_query(query, inverted_index, whole_data):
+    """پرس‌وجو را با مدیریت اولویت‌ها و پرانتزها پردازش می‌کند."""
     tokens = query.lower().split()
-    result_set = set()
-    operator = None
+    operators = []
+    operands = []
 
-    for token in tokens:
-        if token == "and":
-            operator = "and"
-        elif token == "or":
-            operator = "or"
-        elif token == "not":
-            operator = "not"
+    def apply_operator():
+        """عملگر بالای پشته را اجرا کرده و نتیجه را ذخیره می‌کند."""
+        operator = operators.pop()
+        if operator == "not":
+            right_set = operands.pop()
+            operands.append(whole_data - right_set)
         else:
-            # کلمه معمولی: بازیابی اسناد مربوطه
-            current_set = inverted_index.get(token, set())
-            if operator == "not":
-                result_set -= current_set
-            elif operator == "and":
-                result_set &= current_set
-            elif operator == "or" or not result_set:
-                result_set |= current_set
-            else:
-                result_set = current_set
+            right_set = operands.pop()
+            left_set = operands.pop()
+            result = evaluate_operator(operator, left_set, right_set)
+            operands.append(result)
 
-    return result_set
+    precedence = {"not": 3, "and": 2, "or": 1}  # اولویت عملگرها
+    for token in tokens:
+        if token == "(":
+            operators.append(token)
+        elif token == ")":
+            while operators and operators[-1] != "(":
+                apply_operator()
+            operators.pop()
+        elif token in precedence:
+            while (operators and operators[-1] != "(" and
+                   precedence[operators[-1]] >= precedence[token]):
+                apply_operator()
+            operators.append(token)
+        else:
+            operands.append(inverted_index.get(token, set()))
+
+    while operators:
+        apply_operator()
+
+    return operands[-1] if operands else set()
